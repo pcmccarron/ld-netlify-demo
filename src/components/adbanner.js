45c88f8773b60113f26b7bdaa0656de147c218f3
Cody De Arkland <codydearkland@gmail.com>
2022-11-07 22:15:13 -0800

    Go

--- src/components/adbanner.js
+++ src/components/adbanner.js
@@ -5,17 +5,17 @@ import { useState, useEffect } from "react";
 
 export default function Adbanner() {
   const { adbanner } = useFlags()
-  const [maincss, setMainCSS] = useState()
+  const [academyText, setAcademyText] = useState()
 
   const client = useLDClient()
 
   useEffect(() => {
-    setCSS()
+    setText()
     console.log("sending client tracking to LD Experiment for CTA")
     client.track('academy-clickthrough');
   }, [adbanner])
 
-  async function setCSS() {
+  async function setText() {
     await setMainCSS(adbanner)
     console.log(maincss)
     return maincss
@@ -28,11 +28,11 @@ export default function Adbanner() {
 
   return (
     <div
-      className={`p-2 ${maincss} items-center leading-none lg:rounded-full flex lg:inline-flex`}
+      className={`p-2 text-white items-center leading-none lg:rounded-full flex lg:inline-flex`}
       role="alert"
     >
       <span className="font-sohne mr-2 ml-2 text-left flex-auto">
-        How you explored
+        {academyText}
         <a class="cta"
           href="https://academy.launchdarkly.com"
           target="_blank"
