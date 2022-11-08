facb1227dcf2ee086a2499293e182944c3c7f546
Cody De Arkland <codydearkland@gmail.com>
2022-11-07 21:49:34 -0800

    maybe fix

--- src/components/adbanner.js
+++ src/components/adbanner.js
@@ -5,26 +5,10 @@ import { useState, useEffect } from "react";
 
 export default function Adbanner() {
   const { adbanner } = useFlags()
-  const { maincss, setMainCSS } = useState("text-ld-white")
 
   const client = useLDClient()
 
-  function handleAdbanner() {
-    if (adbanner === 'v1') {
-      console.log("eval1")
-      setMainCSS("bg-ldred text-white")
-    } else if (adbanner === 'v2') {
-      console.log("eval2")
-      setMainCSS("bg-ldblue text-white")
-    } else {
-      console.log("eval3")
-      setMainCSS("text-ldwhite")
-    }
-  }
-
   useEffect(() => {
-    handleAdbanner()
-    console.log(maincss)
     console.log("sending client tracking to LD Experiment for CTA")
     client.track('academy-clickthrough');
   }, [adbanner])
@@ -36,7 +20,7 @@ export default function Adbanner() {
 
   return (
     <div
-      className={`p-2 ${maincss} items-center leading-none lg:rounded-full flex lg:inline-flex`}
+      className={`p-2 ${adbanner} items-center leading-none lg:rounded-full flex lg:inline-flex`}
       role="alert"
     >
       <span className="font-sohne mr-2 ml-2 text-left flex-auto">
